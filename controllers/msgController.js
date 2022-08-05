40a7f4eb770d7539b475b925d93c93d402bc04a9
Bioneisme <bioneisme@gmail.com>
2022-08-05 17:38:26 +0600

    Send message to user method

--- controllers/msgController.js
+++ controllers/msgController.js
@@ -1,5 +1,24 @@
-class MsgController {
+const msgMethods = require('../methods/msgMethods')
 
+async function delay(ms) {
+    return await new Promise(resolve => setTimeout(resolve, ms));
+}
+
+class MsgController {
+    async sendMessageToUser(req, res, next) {
+        try {
+            const {user_id, message, count, interval} = req.body;
+            for (let i = 0; i < count; i++) {
+                await delay(interval)
+                const result = await msgMethods.sendMessageToUser(user_id, message);
+                if (result.error) {
+                    return res.render('pages/index', {error: result.message})
+                }
+            }
+        } catch (e) {
+            next(e)
+        }
+    }
 }
 
 module.exports = new MsgController()
--- routes/msgRoutes.js
+++ routes/msgRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const msgController = require('../controllers/msgController');
 
+router.post('/sendMessageToUser', msgController.sendMessageToUser)
 
 module.exports = router
