c01e064bfa5d8866632a53506822f5f9a2e8bca0
Bioneisme <bioneisme@gmail.com>
2022-08-05 15:59:20 +0600

    getAllChats and getDialogs methods

--- methods/msgMethods.js
+++ methods/msgMethods.js
@@ -3,7 +3,7 @@ const api = require('../api')
 const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max));
 
 class msgMethods {
-    async sendMessageToUser(user_id, message) {
+    sendMessageToUser(user_id, message) {
         const params = {
             peer: {
                 _: 'inputPeerUser',
@@ -14,12 +14,35 @@ class msgMethods {
             random_id: getRandomInt(1000000)
         };
 
-        return api.call('messages.sendMessage', params, {}).then((result) => {
-            return result;
-        }).catch(e => {
+        return api.call('messages.sendMessage', params, {})
+        .catch(e => {
             console.error(JSON.stringify(e));
         });
     };
+
+    getAllChats() {
+        return api.call('messages.getAllChats', {
+            except_ids: []
+        }).catch(e => {
+            return {error: true, message: e.error_message}
+        })
+    };
+
+    getDialogs() {
+        return api.call('messages.getDialogs', {
+            exclude_pinned: false,
+            folder_id: 0,
+            offset_date: 0,
+            offset_id: -1,
+            offset_peer: {
+                _: 'inputPeerEmpty'
+            },
+            limit: 100,
+            hash: ""
+        }).catch(e => {
+            return {error: true, message: e.error_message}
+        })
+    }
 }
 
 module.exports = new msgMethods();
