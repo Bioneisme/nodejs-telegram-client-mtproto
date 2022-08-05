8292cc9760633809057d3127831d8a6beb92db5e
Bioneisme <bioneisme@gmail.com>
2022-08-05 15:58:52 +0600

    Initial messages controller & routes

---
+++ controllers/msgController.js
@@ -0,0 +1,5 @@
+class MsgController {
+
+}
+
+module.exports = new MsgController()
---
+++ routes/msgRoutes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const msgController = require('../controllers/msgController');
+
+
+module.exports = router
