a4d6557ff90aee9b8b5f129b48f9f01c7586e27c
Bioneisme <bioneisme@gmail.com>
2022-08-08 10:30:03 +0600

    Initial redis storage

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -4,14 +4,26 @@ dotenv.config();
 
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
 const SERVER_PORT = process.env.SERVER_PORT || 5000;
+const REDIS_HOST = process.env.REDIS_HOST;
+const REDIS_PORT = +(process.env.REDIS_PORT || 13098);
+const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
+const HASH = process.env.HASH
 
 const SERVER = {
     hostname: SERVER_HOSTNAME,
     port: SERVER_PORT
 };
 
+const REDIS = {
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+    password: REDIS_PASSWORD,
+    hash: HASH
+};
+
 const config = {
-    server: SERVER
+    server: SERVER,
+    redis: REDIS
 };
 
 export default config;
